baf9be42b32278bc29df9d29d1fc0b62e0ffb7ee
xristocodos <xristojobos@gmail.com>
2016-05-01 19:26:55 -0400

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,24 @@
+class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+    #does some crazy ish that's purportedly 'new'
+    @coupon = Coupon.new
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.store = params[:store]
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
+
+end
